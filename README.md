7d7f1b70c67f4e499ffce0f5563670354e638707
nomutin <nomura0508@icloud.com>
2021-07-05 22:26:28 +0900

    add readme.md

---
+++ README.md
@@ -0,0 +1,11 @@
+# 都市工学のやつ
+
+## Usage
+日本人口(1986〜2000)
+
+```
+data = CSVTreat.load_data('data/example.csv')
+    mode = ['linear', 'exponential', 'exponential_constant', 'gompertz', 'logistic', 'gompertz_constant']
+    param = Parameters.Param(data=data, mode=mode)
+    Plotting.plot(data=data, param=param.estimated_params, mode=mode, graph_range=40, graph=True, initial=1986)
+```
--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-"""
-Usage:
-    data = CSVTreat.load_data('data/export.csv')
-    mode = ['linear', 'exponential', 'exponential_constant', 'gompertz', 'logistic', 'gompertz_constant']
-    param = Parameters.Param(data=data, mode=mode)
-    Plotting.plot(data=data, param=param.estimated_params, mode=mode, graph_range=40, graph=True, initial=1986)
-
-"""
 from fitting import *
 
 if __name__ == '__main__':
